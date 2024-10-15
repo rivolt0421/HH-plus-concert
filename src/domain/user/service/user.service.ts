ea3ed122a68fbd19f90e55fc56bebf0e35af050e
rivolt0421 <dlrkddnr0421@daum.net>
2024-10-16 05:38:05 +0900

    유저 포인트 - 충전 / 조회 / 사용 서비스 로직 및 레포지토리 인터페이스 작성

--- src/domain/user/service/user.service.ts
+++ src/domain/user/service/user.service.ts
@@ -1,23 +1,77 @@
-import { Inject, NotFoundException } from '@nestjs/common';
+import { ConflictException, Inject, NotFoundException } from '@nestjs/common';
 import {
   USER_READER_REPOSITORY,
   UserReaderRepository,
-} from '../repository/reader.interface';
+} from '../repository/user-reader.interface';
 import { User } from '../entity/user';
+import {
+  USER_WRITER_REPOSITORY,
+  UserWriterRepository,
+} from '../repository/user-writer.interface';
+import {
+  POINT_HISTORY_WRITER_REPOSITORY,
+  PointHistoryWriterRepository,
+} from '../repository/point-history-writer.interface';
 
 export class UserService {
   constructor(
     @Inject(USER_READER_REPOSITORY)
     private readonly userReader: UserReaderRepository,
+    @Inject(USER_WRITER_REPOSITORY)
+    private readonly userWriter: UserWriterRepository,
+    @Inject(POINT_HISTORY_WRITER_REPOSITORY)
+    private readonly pointHistoryWriter: PointHistoryWriterRepository,
   ) {}
 
   async findByEmailAndPassword(email: string, password: string): Promise<User> {
-    const user = await this.userReader.findByEmailAndPassword(email, password);
-
-    if (!user) {
-      throw new NotFoundException('User not exists');
-    }
+    const user = await this.userReader.findByEmailAndPasswordOrThrow(
+      email,
+      password,
+    );
 
     return user;
+  }
+
+  async getPoint(userId: number): Promise<number> {
+    const user = await this.userReader.findByIdOrThrow(userId);
+
+    return user.point;
+  }
+
+  async chargePoint(userId: number, amount: number): Promise<User> {
+    const user = await this.userReader.findByIdOrThrow(userId);
+
+    await this.pointHistoryWriter.save({
+      userId: user.id,
+      amount,
+      type: 'CHARGE',
+      createdAt: new Date(),
+    });
+    const updatedUser = await this.userWriter.incrementPoint(user.id, amount);
+
+    if (!updatedUser) {
+      throw new ConflictException('User point update failed');
+    }
+
+    return updatedUser;
+  }
+
+  async usePoint(userId: number, amount: number): Promise<User> {
+    const user = await this.userReader.findByIdOrThrow(userId);
+    user.validatePointUse(amount);
+
+    await this.pointHistoryWriter.save({
+      userId: user.id,
+      amount,
+      type: 'USE',
+      createdAt: new Date(),
+    });
+    const updatedUser = await this.userWriter.decrementPoint(user.id, amount);
+
+    if (!updatedUser) {
+      throw new ConflictException('User point update failed');
+    }
+
+    return updatedUser;
   }
 }
